0e2ab5b3a86acb929f3dc9214b6ddab25492149e
VandeWinkelGlenn <glenn.van.de.winkel@student.howest.be>
2017-09-26 11:38:23 +0200

    test github

---
+++ hello wolrd.py
@@ -0,0 +1,64 @@
+# print ("hello world")
+#print ("Mijn nam is \"Glenn Van de Winkel")
+
+# bovenstaande regels in 1 regel plaatsen
+#print ("Welkom\n mijn naam is Glenn Van de Wonkel\n 5 september 2017")
+# \n new line, next line
+
+#naam = input ("Geef uw naam op: ")
+#print ("welkom " + naam)
+
+# oef 1print ("Mijn naam is Glenn\n Mijn achternaam is Van de Winkel\nEn mijn emailadres is Glennvandewinkel@gmail.com")
+#print ("labo basic\n\tlabo week 1\n\t\tKenninsmaking\n\t\twerken met variabelen\nLabo basic 2\n\t labo week 2")
+
+#wekern ,et variabelen
+#naam = input ("Wat is uw naam: ")
+#commando int gaat tekst string o,zetten naar een getal int
+#leeftijd = int(input("Wat is uw leeftijd: "))
+
+#verjaardag
+#leeftijd = leeftijd + 1
+
+#via placeholders kan ik mijn tekst opvullen met data uit variabelen
+#print ("Hallo %s. U bent %d jaar." % (naam, leeftijd))
+
+#alternatieve manier:
+#print("welkom (0). U bent (1) jaar.") .format (naam, leeftijd)
+#print("welkom (1). U bent (0) jaar.") .format (naam, leeftijd)
+
+# opvragen van commagetal
+#lengte = float(input ("geef uw lengte op in meter: "))
+#lengte = float(input("geef uw lengte: "))
+#print("u bent {0 : 8.2f%} meter lang".format(lengte))
+
+#doe het maar op deze manier
+#print("u bent %f meter lang" % (lengte))
+
+#
+# Basis = float(input("Hoeveel centimeter is de basis: "))
+# Hoogte = float(input("hoeveel centimeter is de hoogte:"))
+# opp_driehoek =  Basis * Hoogte / 2
+# print("De oppervlakte van uw driehoek is {0:.2f} vierkante centimter".format(opp_driehoek))
+#
+# tijd = float(input("Geef mij een aantal seconden: "))
+# d = tijd //86400
+# rest = tijd % 86400
+#
+# # # // alles na comma weg
+#
+# koeken = int(input("Hoeveel koeken hebt u besteld? "))
+# prijs = int(input("hoeveel hebt u betaald voor 1 koek? "))
+# totaal = prijs * koeken
+#
+# print("u hebt %s koeken.\nU hebt %s euro betaald voor 1 koek.\nU heeft in totaal %s aan koeken gekocht" %(koeken, prijs, totaal))
+
+# Getal = int(input("Hoeveel is 4+1?"))
+# if (Getal == 5):
+#     print("Juist")
+#
+# else :
+#     print("Fout, probeer opnieuw.")
+
+# glenn = "hlenn"
+# print(glenn)
+
